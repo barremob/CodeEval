e89544ef6a45cf052e0e870d5837822e7a8797f6
Barremob <everaerts.bart@gmail.com>
2015-11-06 21:50:35 +0100

    New test of Delta Time, less memory

--- CSharp/Easy/DeltaTime.cs
+++ CSharp/Easy/DeltaTime.cs
@@ -1,8 +1,11 @@
 ﻿/*
+Different test, the last one used to much memory (compared to other tests!)
+Could improve more, but who cares.
+
 Score:      100
-Time:       373
-Memory:     5582848
-Points:     29.688
+Time:       186
+Memory:     4878336
+Points:     30.602
 */
 
 using System;
@@ -24,25 +27,58 @@ class Program
 				}
 
 				string[] inputData = line.Split(' ');
-				DateTime[] Times = new DateTime[2];
 
-				for (int i = 0; i < inputData.Length; i++)
-				{
-					Times[i] = Convert.ToDateTime(inputData[i]);
-				}
-
-				if (Times[0] > Times[1])
-				{
-					Console.WriteLine(Times[0] - Times[1]);
-				}
-				else
-				{
-					Console.WriteLine(Times[1] - Times[0]);
-				}
+				Console.WriteLine(DeltaTime(inputData[0], inputData[1]));
 			}
 		}
 
 		Console.ReadKey();
 	}
-}
 
+	static string DeltaTime(string a, string b)
+	{
+		int[] time = new int[2];
+		time[0] = StrTimeToSeconds(a);
+		time[1] = StrTimeToSeconds(b);
+
+		int diff = 0;
+
+		if (time[0] > time[1])
+		{
+			diff = time[0] - time[1];
+		}
+		else
+		{
+			diff = time[1] - time[0];
+		}
+
+		string rtnTime = SecondsToTimeStr(diff);
+
+		return rtnTime;
+	}
+
+	static int StrTimeToSeconds(string time)
+	{
+		string[] _timeData = time.Split(':');
+		int[] _time = Array.ConvertAll<string, int>(_timeData, int.Parse);
+		int rtnSeconds = 0;
+
+		rtnSeconds += _time[0] * 3600;
+		rtnSeconds += _time[1] * 60;
+		rtnSeconds += _time[2];
+
+		return rtnSeconds;
+	}
+
+	static string SecondsToTimeStr(int seconds)
+	{
+		int[] _time = new int[3];
+		int remainder = seconds;
+		_time[0] = remainder / 3600;
+		remainder -= _time[0] * 3600;
+		_time[1] = remainder / 60;
+		remainder -= _time[1] * 60;
+		_time[2] = remainder;
+		return string.Format("{0:D2}:{1:D2}:{2:D2}", _time[0], _time[1], _time[2]);
+	}
+}
