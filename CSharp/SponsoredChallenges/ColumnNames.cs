6ee239ddd5052f9572338239739a4009b532c14c
Barremob <everaerts.bart@gmail.com>
2015-11-10 16:47:50 +0100

    Column Names - Blackbaud

---
+++ CSharp/SponsoredChallenges/ColumnNames.cs
@@ -0,0 +1,44 @@
+﻿/*
+Column Names - Blackbaud
+
+Score:      100
+Time:       159
+Memory:     4755456
+Points:     57.111
+*/
+
+using System.IO;
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		//using (StreamReader reader = File.OpenText(args[0]))
+		using (StreamReader reader = File.OpenText(@"SponsoredChallenges\ColumnNames.txt"))
+		{
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+				{
+					continue;
+				}
+				
+				int columnNumber = Convert.ToInt32(line);
+				string rtn = "";
+
+				while (columnNumber>0)
+				{
+					columnNumber--;
+					rtn = (char)(65 + columnNumber % 26)+ rtn;
+					columnNumber /= 26;
+				}
+
+				Console.WriteLine(rtn);
+			}
+		}
+
+		Console.ReadKey();
+	}
+}
