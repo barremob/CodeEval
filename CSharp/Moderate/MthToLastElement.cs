cc4ff57c643e00e2a5421a5c20d0af628a57dd74
Barremob <everaerts.bart@gmail.com>
2015-11-14 15:57:07 +0100

    Mth To Last Element

---
+++ CSharp/Moderate/MthToLastElement.cs
@@ -0,0 +1,41 @@
+﻿/*
+Score:      100
+Time:       156
+Memory:     4718592
+Points:     57.178
+*/
+
+using System.IO;
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		//using (StreamReader reader = File.OpenText(args[0]))
+		using (StreamReader reader = File.OpenText(@"Moderate\MthToLastElement.txt"))
+		{
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line || line == "")
+				{
+					continue;
+				}
+
+				string[] data = line.Split(' ');
+				int mthNbr = Convert.ToInt32(data[data.Length - 1]);
+				if (mthNbr > data.Length - 1)
+				{
+					continue;
+				}
+
+				int rtnIndex = (data.Length - 1) - mthNbr;
+
+				Console.WriteLine(data[rtnIndex]);
+			}
+		}
+
+		Console.ReadKey();
+	}
+}
