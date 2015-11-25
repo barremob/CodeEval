f397bf1b74b78e25e8c9f04861085a4df3f9451e
Barremob <everaerts.bart@gmail.com>
2015-11-25 14:25:34 +0100

    Endianness

---
+++ CSharp/Moderate/Endianness.cs
@@ -0,0 +1,25 @@
+﻿/*
+Score:      100
+Time:       101
+Memory:     4562944
+Points:     57.600
+*/
+
+using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		ushort number = 123;
+		byte upper = (byte)(number >> 4);
+		
+		if (upper != 7)
+		{
+			Console.WriteLine("BigEndian");
+		}
+		else
+		{
+			Console.WriteLine("LittleEndian");
+		}
+	}
+}
