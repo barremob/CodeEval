57cd0371824c1f9b3580b5618b268e95aff3b31d
Barremob <everaerts.bart@gmail.com>
2015-12-04 22:32:32 +0100

    Decimal To Binary

---
+++ CSharp/Moderate/DecimalToBinary.cs
@@ -0,0 +1,33 @@
+﻿/*
+Score:      100
+Time:       160
+Memory:     4788224
+Points:     57.058
+*/
+
+using System.IO;
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		//using (StreamReader reader = File.OpenText(args[0]))
+		using (StreamReader reader = File.OpenText(@"Moderate\DecimalToBinary.txt"))
+		{
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line || line == "")
+				{
+					continue;
+				}
+				
+				string binary = Convert.ToString(Convert.ToInt32(line), 2);
+				Console.WriteLine(binary);
+			}
+		}
+
+		Console.ReadKey();
+	}
+}
