f32a43b2e57679fb7a029f3fa03629353c0e592c
Barremob <everaerts.bart@gmail.com>
2015-12-05 22:58:38 +0100

    Pangrams

---
+++ CSharp/Moderate/Pangrams.cs
@@ -0,0 +1,59 @@
+﻿/*
+Score:      100
+Time:       167
+Memory:     4845568
+Points:     56.945
+*/
+
+using System.IO;
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		//using (StreamReader reader = File.OpenText(args[0]))
+		using (StreamReader reader = File.OpenText(@"Moderate\Pangrams.txt"))
+		{
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line || line == "")
+				{
+					continue;
+				}
+
+				bool[] alpha = new bool[26];
+				char[] chars = line.ToLower().ToCharArray();
+
+				for (int i = 0; i < chars.Length; i++)
+				{
+					if (chars[i] >= 97 && chars[i] <= 122)
+					{
+						int index = chars[i] - 97;
+						alpha[index] = true;
+					}
+				}
+
+				string rtn = "";
+
+				for (int i = 0; i < 26; i++)
+				{
+					if (alpha[i] == false)
+					{
+						rtn += (char)(i + 97);
+					}
+				}
+
+				if (rtn == "")
+				{
+					rtn = "NULL";
+				}
+
+				Console.WriteLine(rtn);
+			}
+		}
+
+		Console.ReadKey();
+	}
+}
